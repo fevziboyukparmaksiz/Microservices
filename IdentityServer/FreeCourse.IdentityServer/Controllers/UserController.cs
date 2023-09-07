4f94a55bdbc3a78833c8f35434745d12c7290771
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-09-07 17:39:37 +0300

    UserController/Signup endpoint test işlemi

--- IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
+++ IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
@@ -3,21 +3,29 @@ using System.Threading.Tasks;
 using FreeCourse.IdentityServer.Dtos;
 using FreeCourse.IdentityServer.Models;
 using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using static IdentityServer4.IdentityServerConstants;
 
 namespace FreeCourse.IdentityServer.Controllers
 {
-    [Route("api/[controller]")]
+    [Authorize(LocalApi.PolicyName)]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class UserController : ControllerBase
     {
         private readonly UserManager<ApplicationUser> _userManager;
 
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         [HttpPost]
         public async Task<IActionResult> SignUp(SignUpDto signUpDto)
         {
-            var user = new ApplicationUser()
+            var user = new ApplicationUser
             {
                 UserName = signUpDto.UserName,
                 Email = signUpDto.Email,
