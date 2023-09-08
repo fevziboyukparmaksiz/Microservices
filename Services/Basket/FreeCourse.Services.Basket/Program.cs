a5d666e05ec88e0866184255237e576269875e1e
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-09-08 18:03:14 +0300

    Appsettings.json > Host ve Port bilgilerini belirleme

--- Services/Basket/FreeCourse.Services.Basket/Program.cs
+++ Services/Basket/FreeCourse.Services.Basket/Program.cs
@@ -1,9 +1,15 @@
+using FreeCourse.Services.Basket.Settings;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<RedisSettings>(builder.Configuration.GetSection("RedisSettings"));
+
+
 
 var app = builder.Build();
 
