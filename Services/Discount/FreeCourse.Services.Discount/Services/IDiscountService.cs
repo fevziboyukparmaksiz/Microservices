9bf734c76667ba111ec7f54a1b6b359bb75853d3
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-09-10 15:13:15 +0300

    DiscountService

--- Services/Discount/FreeCourse.Services.Discount/Services/IDiscountService.cs
+++ Services/Discount/FreeCourse.Services.Discount/Services/IDiscountService.cs
@@ -1,6 +1,14 @@
-﻿namespace FreeCourse.Services.Discount.Services
+﻿using FreeCourse.Shared.Dtos;
+
+namespace FreeCourse.Services.Discount.Services
 {
-    public class DiscountService
+    public interface IDiscountService
     {
+        Task<Response<List<Models.Discount>>> GetAll();
+        Task<Response<Models.Discount>> GetById(int id);
+        Task<Response<NoContent>> Save(Models.Discount discount);
+        Task<Response<NoContent>> Update(Models.Discount discount);
+        Task<Response<NoContent>> Delete(int id);
+        Task<Response<Models.Discount>> GetByCodeAndUserId(string code, string userId);
     }
 }
