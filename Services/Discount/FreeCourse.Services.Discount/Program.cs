f04e215fb447678aebf1c0e4fd0a9821ce021790
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-09-11 21:03:02 +0300

    Discount microservice koruma altına alma

--- Services/Discount/FreeCourse.Services.Discount/Program.cs
+++ Services/Discount/FreeCourse.Services.Discount/Program.cs
@@ -1,9 +1,31 @@
+using System.IdentityModel.Tokens.Jwt;
+using FreeCourse.Services.Discount.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+});
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServerURL"];
+        options.Audience = "resource_discount";
+        options.RequireHttpsMetadata = false;
+    });
+
+builder.Services.AddScoped<IDiscountService, DiscountService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -16,6 +38,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
