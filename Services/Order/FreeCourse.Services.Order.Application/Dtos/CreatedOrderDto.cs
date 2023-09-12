ebddd65390f0da5486459ba4b980447941da875e
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-09-13 01:43:52 +0300

    AutoMapper

--- Services/Order/FreeCourse.Services.Order.Application/Dtos/CreatedOrderDto.cs
+++ Services/Order/FreeCourse.Services.Order.Application/Dtos/CreatedOrderDto.cs
@@ -1,4 +1,10 @@
-﻿namespace FreeCourse.Services.Order.Application.Dtos
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Application.Dtos
 {
     public class CreatedOrderDto
     {
