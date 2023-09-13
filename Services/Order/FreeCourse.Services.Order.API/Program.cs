1e5b5cab500f05c690e904ed68aa10cd20e251ee
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-09-13 22:21:43 +0300

    Migrations

--- Services/Order/FreeCourse.Services.Order.API/Program.cs
+++ Services/Order/FreeCourse.Services.Order.API/Program.cs
@@ -1,11 +1,22 @@
+using FreeCourse.Services.Order.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<OrderDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), configure =>
+    {
+        configure.MigrationsAssembly("FreeCourse.Services.Order.Infrastructure");
+    });
+});
+
+
 
 var app = builder.Build();
 
