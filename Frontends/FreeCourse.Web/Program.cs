02d5fdda7e00539a585920c388c4c30dde8f8a9d
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-09-15 00:10:40 +0300

    ServiceApiSettings

--- Frontends/FreeCourse.Web/Program.cs
+++ Frontends/FreeCourse.Web/Program.cs
@@ -1,11 +1,15 @@
+using FreeCourse.Web.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.Configure<ServiceApiSettings>(builder.Configuration.GetSection("ServiceApiSetting"));
+
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
