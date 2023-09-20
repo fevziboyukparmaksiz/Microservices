4bc430bca69a454191ab18f2a73ddeeadbc55eef
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-09-21 00:46:00 +0300

    Basket microservice’i için viewmodel oluşturma

---
+++ Frontends/FreeCourse.Web/Models/Baskets/BasketItemViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace FreeCourse.Web.Models.Baskets
+{
+    public class BasketItemViewModel
+    {
+        public int Quantity { get; set; }
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+        public decimal Price { get; set; }
+
+        private decimal? DicountAppliedPrice { get; set; }
+
+        public decimal GetCurrentPrice => DicountAppliedPrice != null ? DicountAppliedPrice.Value : Price;
+
+        public void AppliedDiscount(decimal discountPrice)
+        {
+            DicountAppliedPrice = discountPrice;
+        }
+    }
+}
---
+++ Frontends/FreeCourse.Web/Models/Baskets/BasketViewModel.cs
@@ -0,0 +1,36 @@
+﻿namespace FreeCourse.Web.Models.Baskets
+{
+    public class BasketViewModel
+    {
+        public string UserId { get; set; }
+        public string DiscountCode { get; set; }
+        public int? DiscountRate { get; set; }
+        private List<BasketItemViewModel> _basketItems { get; set; }
+
+        public List<BasketItemViewModel> BasketItem
+        {
+            get
+            {
+                if (hasDiscount)
+                {
+                    _basketItems.ForEach(x =>
+                    {
+                        var discountPrice = x.Price - ((decimal)DiscountRate.Value / 100);
+                        x.AppliedDiscount(Math.Round(x.Price - discountPrice, 2));
+                    });
+                }
+
+                return _basketItems;
+            }
+
+            set
+            {
+                _basketItems = value;
+            }
+        }
+
+        public decimal TotalPrice => _basketItems.Sum(x => x.GetCurrentPrice);
+
+        public bool hasDiscount => !string.IsNullOrEmpty(DiscountCode);
+    }
+}
