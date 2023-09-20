f21ca3cdcc5db001c137d37ea1423ea5751f9caf
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-09-20 19:51:55 +0300

    IPhotoStockService/PhotoStockService-2

--- Frontends/FreeCourse.Web/Services/PhotoStockService.cs
+++ Frontends/FreeCourse.Web/Services/PhotoStockService.cs
@@ -5,14 +5,43 @@ namespace FreeCourse.Web.Services
 {
     public class PhotoStockService : IPhotoStockService
     {
+        private readonly HttpClient _httpClient;
+
+        public PhotoStockService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
         public async Task<PhotoViewModel> UploadPhoto(IFormFile photo)
         {
-            throw new NotImplementedException();
+            if (photo == null && photo.Length <= 0)
+            {
+                return null;
+            }
+
+            var randomFileName = $"{Guid.NewGuid().ToString()}{Path.GetExtension(photo.FileName)}";
+
+            using var ms = new MemoryStream();
+
+            await photo.CopyToAsync(ms);
+
+            var multiPartContent = new MultipartFormDataContent();
+            multiPartContent.Add(new ByteArrayContent(ms.ToArray()), "photo", randomFileName);
+
+            var response = await _httpClient.PostAsync("photo", multiPartContent);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            return await response.Content.ReadFromJsonAsync<PhotoViewModel>();
         }
 
         public async Task<bool> DeletePhoto(string photoUrl)
         {
-            throw new NotImplementedException();
+            var response = await _httpClient.DeleteAsync($"photo?photoUrl={photoUrl}");
+            return response.IsSuccessStatusCode;
         }
     }
 }
