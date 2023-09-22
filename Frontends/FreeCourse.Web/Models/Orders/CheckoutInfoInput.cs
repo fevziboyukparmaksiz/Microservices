adc776e7e35bb6d9c3b42019a8d5e1a9c5c1ef19
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-09-23 02:39:18 +0300

    Order Microservice için CheckoutInfoInput

---
+++ Frontends/FreeCourse.Web/Models/Orders/CheckoutInfoInput.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace FreeCourse.Web.Models.Orders
+{
+    public class CheckoutInfoInput
+    {
+        [DisplayName("İl")]
+        public string Province { get; set; }
+        [DisplayName("İlçe")]
+        public string District { get; set; }
+        [DisplayName("Cadde")]
+        public string Street { get; set; }
+        [DisplayName("Posta Kodu")]
+        public string ZipCode { get; set; }
+        [DisplayName("Adres")]
+        public string Line { get; set; }
+        [DisplayName("Kart Ad Soyad")]
+        public string CardName { get; set; }
+        [DisplayName("Kart Numarası")]
+        public string CardNumber { get; set; }
+        [DisplayName("Son Kullanma Tarih(ay/yıl) ")]
+        public string Expiration { get; set; }
+        [DisplayName("CVV/CVC2")]
+        public string CVV { get; set; }
+    }
+}
