9cb46a9483be1eb1bbfffaf4d37ba78c0483d68e
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-09-23 22:32:28 +0300

    OrderController /Checkout.cshtml

---
+++ Frontends/FreeCourse.Web/Controllers/OrderController.cs
@@ -0,0 +1,50 @@
+﻿using FreeCourse.Web.Models.Orders;
+using FreeCourse.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderService _orderService;
+        private readonly IBasketService _basketService;
+
+        public OrderController(IOrderService orderService, IBasketService basketService)
+        {
+            _orderService = orderService;
+            _basketService = basketService;
+        }
+
+        public async Task<IActionResult> Checkout()
+        {
+            var basket = await _basketService.Get();
+            ViewBag.basket = basket;
+            return View(new CheckoutInfoInput());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Checkout(CheckoutInfoInput checkoutInfoInput)
+        {
+            var orderStatus = await _orderService.CreateOrder(checkoutInfoInput);
+
+            if (!orderStatus.IsSuccessful)
+            {
+                var basket = await _basketService.Get();
+
+                ViewBag.basket = basket;
+                ViewBag.error = orderStatus.Error;
+
+                return View();
+            }
+
+            return RedirectToAction(nameof(SuccessfulCheckout), new { orderId = orderStatus.OrderId });
+
+        }
+
+        public async Task<IActionResult> SuccessfulCheckout(int orderId)
+        {
+            ViewBag.orderId = orderId;
+            return View();
+        }
+    }
+}
